ca6246df814f547e0db149c47cdc1a98c9ac26ac
chicken1001 <48146004+chicken1001@users.noreply.github.com>
2025-11-30 17:09:52 -0500

    remove old lib/supabase.ts

--- lib/supabase.ts
+++
@@ -1,5 +0,0 @@
-// lib/supabase.ts
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { Database } from '@/types/supabase'
-
-export const createClient = () => createClientComponentClient<Database>()
